c10aca4f601cc16a63c0182bd60c48966c130a7f
Vinz2008 <clubfunreading@@gmail.com>
2021-09-06 21:33:00 +0200

    Used PyInquirer for the input

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from googlesearch import search
-
-query = input("What is your search ? ")
+from PyInquirer import prompt
+questions = [
+    {
+        'type': 'input',
+        'name': 'search_input',
+        'message': 'What is your search ? ',
+    }
+]
+answers = prompt(questions)
+query = answers.get("search_input")
 for j in search(query, tld="co.in", num=10, stop=10,pause=2):
     print(j)
